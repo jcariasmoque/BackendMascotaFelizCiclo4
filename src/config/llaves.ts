444263eb05a1051c093955242175fd53a81f88fe
jcariasmoque <jariasmoque@hotmail.com>
2022-11-24 14:08:40 -0500

    updatellaves

--- src/config/llaves.ts
+++ src/config/llaves.ts
@@ -1,4 +1,4 @@
 export namespace Llaves{
     export const claveJWT = 'Juan1234*';
-    export const urlServicioNotificaciones ='http://localhost:5000';
+    export const urlServicioNotificaciones ='http://127.0.0.1:5000';
 }
